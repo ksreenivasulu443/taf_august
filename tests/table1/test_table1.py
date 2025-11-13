ccc80d9093f7605b30376bcb797b2035f05d0f6c
admin <kattubadi426@gmail.com>
2025-11-13 07:22:54 +0530

    New commit

--- tests/table1/test_table1.py
+++ tests/table1/test_table1.py
@@ -1,8 +1,15 @@
 from src.data_validations.count_check import count_check
+from src.data_validations.duplicate_check import duplicate_check
 
 
 def test_count(read_data):
     source_df, target_df, validation_config = read_data
     key_columns = validation_config['count_check']['key_columns']
     status = count_check(source_df=source_df, target_df=target_df, key_columns=key_columns)
+    assert status.upper() == 'PASS'
+
+def test_duplicate(read_data):
+    source_df, target_df, validation_config = read_data
+    key_columns = validation_config['duplicate_check']['key_columns']
+    status = duplicate_check(df=target_df, key_col=key_columns)
     assert status.upper() == 'PASS'
